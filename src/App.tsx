e7cd7534dd2936f847f24e71dd0224a28819faea
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-07-09 15:51:15 +0100

    chore: install vite, react, ts, swc, papaparse

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import pokemonData from './data/pokekmon_cleaned.csv'
+import Papa from 'papaparse'
+function App() {
+  Papa.parse
+
+  return (
+    <div>Hello world</div>
+  )
+}
+
+export default App
