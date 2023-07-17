3c780eb9fd8bf5b8b68b97ab725364a1880a9ec3
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-07-17 23:30:04 +0100

    feat: add tabs to pokecard

--- src/components/PokeCard.tsx
+++ src/components/PokeCard.tsx
@@ -19,6 +19,9 @@ ChartJS.register(
   Legend
 );
 
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+
 const fields = ["HP", "Attack", "Defense", "Special Attack", "Special Defense", "Speed"];
 
 
@@ -43,60 +46,74 @@ const PokeCard = ({pokemon}: {pokemon: MyPokemon}) => {
     <div className="max-w-sm rounded-xl overflow-hidden shadow-xl hover:shadow-gradient-to-r hover:-translate-y-1 ease-in-out transition-all duration-[350ms]">
       <div className="py-2">
         <div className="px-4 flex justify-between items-center gap-2 bg-gray-300 py-1 rounded-t-xl">
-        <div className="font-bold text-xl">{pokemon.Names}</div>
-      <div className=" grid relative w-24 h-24">
-        <img
-          className="w-full object-contain relative top-[-30px]"
-          src={pokemon.image_url}
-          alt="Sunset in the mountains"
-        />
-      </div>
+          <div className="font-bold text-xl">{pokemon.Names}</div>
+          <div className=" grid relative w-24 h-24">
+            <img
+              className="w-full object-contain relative top-[-30px]"
+              src={pokemon.image_url}
+              alt="Sunset in the mountains"
+            />
+          </div>
+        </div>
+        <div className="not-prose grid items-center">
+          <Tabs selectedTabClassName="" >
+            <TabList >
+              <Tab>Distribution</Tab>
+              <Tab>Tabular</Tab>
+            </TabList>
+            <TabPanel>
+              <Radar data={data} />
+            </TabPanel>
 
+              <TabPanel >
+                <table className="table-auto w-full">
+                  <thead>
+                    <tr>
+                      <th className="px-4 py-2">Attribute</th>
+                      <th className="px-4 py-2">Value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="border px-4 py-2">HP</td>
+                      <td className="border px-4 py-2">{pokemon.HP}</td>
+                    </tr>
+                    <tr className="bg-gray-100">
+                      <td className="border px-4 py-2">Attack</td>
+                      <td className="border px-4 py-2">{pokemon.Attack}</td>
+                    </tr>
+                    <tr>
+                      <td className="border px-4 py-2">Defense</td>
+                      <td className="border px-4 py-2">{pokemon.Defense}</td>
+                    </tr>
+                    <tr className="bg-gray-100">
+                      <td className="border px-4 py-2">Special Attack</td>
+                      <td className="border px-4 py-2">
+                        {pokemon["Special Attack"]}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="border px-4 py-2">Special Defense</td>
+                      <td className="border px-4 py-2">
+                        {pokemon["Special Defense"]}
+                      </td>
+                    </tr>
+                    <tr className="bg-gray-100">
+                      <td className="border px-4 py-2">Speed</td>
+                      <td className="border px-4 py-2">{pokemon.Speed}</td>
+                    </tr>
+                  </tbody>
+                  <tfoot>
+                    <tr>
+                      <td className="border px-4 py-2 font-bold uppercase">Total</td>
+                      <td className="border px-4 py-2 font-bold">{pokemon.Total}</td>
+                    </tr>
+                  </tfoot>
+                </table>
+              </TabPanel>
+
+          </Tabs>
         </div>
-        <div className="grid items-center pr-8">
-        <Radar data={data} />
-        </div>
-        {/*
-        <table className="hidden table-auto my-1">
-          <thead>
-            <tr>
-              <th className="px-4 py-2">Attribute</th>
-              <th className="px-4 py-2">Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="border px-4 py-2">HP</td>
-              <td className="border px-4 py-2">{pokemon.HP}</td>
-            </tr>
-            <tr className="bg-gray-100">
-              <td className="border px-4 py-2">Attack</td>
-              <td className="border px-4 py-2">{pokemon.Attack}</td>
-            </tr>
-            <tr>
-              <td className="border px-4 py-2">Defense</td>
-              <td className="border px-4 py-2">{pokemon.Defense}</td>
-            </tr>
-            <tr className="bg-gray-100">
-              <td className="border px-4 py-2">Special Attack</td>
-              <td className="border px-4 py-2">{pokemon["Special Attack"]}</td>
-            </tr>
-            <tr>
-              <td className="border px-4 py-2">Special Defense</td>
-              <td className="border px-4 py-2">{pokemon["Special Defense"]}</td>
-            </tr>
-            <tr className="bg-gray-100">
-              <td className="border px-4 py-2">Speed</td>
-              <td className="border px-4 py-2">{pokemon.Speed}</td>
-            </tr>
-          </tbody>
-          <tfoot>
-            <tr>
-              <td className="border px-4 py-2">Total</td>
-              <td className="border px-4 py-2">{pokemon.Total}</td>
-            </tr>
-          </tfoot>
-        </table>*/}
       </div>
       <div className="px-6  pb-2">
         <span
